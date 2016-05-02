4d658101c3d8c36002afddb3636699bdfe5e4fbe
Svilcata <lord_solen@abv.bg>
2016-05-03 00:04:59 +0300

    Chapter 3 - finished with this sample app for fragments

---
+++ app/src/main/java/com/svilcata/fragmentexample/FragmentAnalog.java
@@ -0,0 +1,18 @@
+package com.svilcata.fragmentexample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Svilcata on 02-May-16.
+ */
+public class FragmentAnalog extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_analog, container, false);
+    }
+}
